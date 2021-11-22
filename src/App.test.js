f34375919c92cd0aa5e5c48756fe0ce86c87e2f8
mickoangelolacap <mickoangelolacap@gmail.com>
2021-11-22 19:19:05 +0800

    Test and code function to replace camel-case colors with spaces

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { replaceCamelWithSpaces } from './App'
 
 test('button has correct initial color', () => {
   render(<App />)
@@ -47,4 +48,19 @@ test('onchange conditions', () => {
   expect(colorButton).toBeEnabled()
   expect(colorButton).toHaveStyle({backgroundColor: 'red'})
   expect(checkbox).not.toBeChecked()
+})
+
+describe('spaces before camel-case capital letters', () => {
+
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red')
+  })
+
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue')
+  })
+
+  test('Works for multiple inner capital letters',() => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red')
+  })
 })
